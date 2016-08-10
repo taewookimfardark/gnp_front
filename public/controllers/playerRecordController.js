6dacafc988c5d84e858ccc7263ce088a20749962
taewookimfardark <2012142087@yonsei.ac.kr>
2016-08-10 15:09:53 +0900

    선수기록 페이지 완료

--- public/controllers/playerRecordController.js
+++ public/controllers/playerRecordController.js
@@ -1,19 +1,20 @@
 gnp_app.controller('playerRecordController', ["$scope", "httpRequest", function ($scope, httpRequest) {
 
-    $scope.playerrecord = []
-    httpRequest.send('GET','users')
+    $scope.playerrecord = [];
+    httpRequest.send('GET','playerrecord/page')
         .then(
             function(res)
             {
-                for(var index in res.data)
+                console.log(res);
+                for(var index in res.data.data)
                 {
                     var temp = {"Name" :'',"Backnumber":'',"Games":'',"Points":'',"Rebounds":'',"Assists":''}
-                    temp.Name = res.data[index].name;
-                    temp.Backnumber = res.data[index].backnumber;
-                    temp.Games = res.data[index].records.games;
-                    temp.Points = parseFloat(res.data[index].records.points/temp.Games).toFixed(2);
-                    temp.Rebounds = parseFloat(res.data[index].records.rebounds/temp.Games).toFixed(2);
-                    temp.Assists = parseFloat(res.data[index].records.assists/temp.Games).toFixed(2);
+                    temp.Name = res.data.data[index][0].name;
+                    temp.Backnumber = res.data.data[index][0].backnumber;
+                    temp.Games = res.data.data[index][1].games;
+                    temp.Points = parseFloat(res.data.data[index][1].points/temp.Games).toFixed(2);
+                    temp.Rebounds = parseFloat(res.data.data[index][1].rebounds/temp.Games).toFixed(2);
+                    temp.Assists = parseFloat(res.data.data[index][1].assists/temp.Games).toFixed(2);
                     
                     $scope.playerrecord.push(temp);
                     
